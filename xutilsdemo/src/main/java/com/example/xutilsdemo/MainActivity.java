e33caa74e3cb08610efe789c75afc95f4ad89175
xiaofen <247690790@qq.com>
2016-06-01 19:50:27 +0800

    update 2016-06-01 19:48:31

--- xutilsdemo/src/main/java/com/example/xutilsdemo/MainActivity.java
+++ xutilsdemo/src/main/java/com/example/xutilsdemo/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.xutilsdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.xutils.view.annotation.ContentView;
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.http.annotation.HttpRequest;
+import org.xutils.http.app.DefaultParamsBuilder;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @ViewInject(R.id.textView)
     private TextView textView;
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +45,100 @@ public class MainActivity extends AppCompatActivity {
         /*View注入*/
         x.view().inject(this);
         textView.setText("************");
+    }
+
+    /*文件上传*/
+    @Event(value = {R.id.textView, R.id.button}, type = View.OnClickListener.class)
+    private void ss(View view) {
+
+        if (path != null) {
+            Log.e("path", path);
+            String uploadHost = "http://192.168.1.200:8080/T/U"; //服务器接收地址
+
+            RequestParams params = new RequestParams(uploadHost);
+            // 加到url里的参数, http://xxxx/s?wd=xUtils
+            params.addQueryStringParameter("wd", "xUtils");
+            // 添加到请求body体的参数, 只有POST, PUT, PATCH, DELETE请求支持.
+            // params.addBodyParameter("wd", "xUtils");
+            // 使用multipart表单上传文件
+            params.setMultipart(true);
+            params.addBodyParameter(
+                    "img",
+                    new File(path),
+                    null); // 如果文件没有扩展名, 最好设置contentType参数.
+            x.http().post(params, new Callback.CommonCallback<String>() {
+                @Override
+                public void onSuccess(String result) {
+                    Toast.makeText(x.app(), result, Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onError(Throwable ex, boolean isOnCallback) {
+                    Toast.makeText(x.app(), ex.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onCancelled(CancelledException cex) {
+                    Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onFinished() {
+
+                }
+            });
+        } else {
+            Toast.makeText(this, "path is null", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
 
 
-    @Event(value = {R.id.textView,R.id.button},type= View.OnClickListener.class)
-    private void ss(View view){
 
-        Toast.makeText(this,"************",Toast.LENGTH_SHORT).show();
+
+    private final String IMAGE_TYPE = "image/*";
+    private final int IMAGE_CODE = 0;   //这里的IMAGE_CODE是自己任意定义的
+
+    public void select(View view) {
+        Intent getAlbum = new Intent(Intent.ACTION_GET_CONTENT);
+        getAlbum.setType(IMAGE_TYPE);
+        startActivityForResult(getAlbum, IMAGE_CODE);
 
     }
+    /*获取图片路径*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {        //此处的 RESULT_OK 是系统自定义得一个常量
+            Log.e("tag", "ActivityResult resultCode error");
+            return;
+        }
 
+        Bitmap bm = null;
 
+        //外界的程序访问ContentProvider所提供数据 可以通过ContentResolver接口
+        ContentResolver resolver = getContentResolver();
 
+        //此处的用于判断接收的Activity是不是你想要的那个
+        if (requestCode == IMAGE_CODE) {
+            try {
+                Uri originalUri = data.getData();        //获得图片的uri
 
+                bm = MediaStore.Images.Media.getBitmap(resolver, originalUri);        //显得到bitmap图片这里开始的第二部分，获取图片的路径：
+
+                String[] proj = {MediaStore.Images.Media.DATA};
+
+                //好像是Android多媒体数据库的封装接口，具体的看Android文档
+                Cursor cursor = managedQuery(originalUri, proj, null, null, null);
+                //按我个人理解 这个是获得用户选择的图片的索引值
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                //将光标移至开头 ，这个很重要，不小心很容易引起越界
+                cursor.moveToFirst();
+                //最后根据索引值获取图片路径
+                path = cursor.getString(column_index);
+            } catch (Exception e) {
+                Log.e("tag", e.toString());
+            }
+        }
+    }
 }
